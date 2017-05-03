a1af435ef1f191ea576f0df7471eb8f00f0905e1
Till Pohland <till.pohland@gmx.de>
2017-05-03 22:34:24 +0200

    Testframe riesen update

--- a2-chat/src/TestFrame.java
+++ a2-chat/src/TestFrame.java
@@ -6,16 +6,15 @@ package src;
 public class TestFrame {
 
     public static void main(String[] args) {
-        ChatServer server;
         int port = 13000;
         String serverOrClient = "0";
 
         if(serverOrClient == "1"){
-            server = new ChatServer(port);//Integer.parseInt(args[1]));
+            new ChatServer(port);//Integer.parseInt(args[1]));
             System.out.println("Server could not be created or has been shutdown");
         } else {
             ChatClient client = new ChatClient();
-            client.login("",port);
+            client.login("localhost",port);
         }
     }
 }
