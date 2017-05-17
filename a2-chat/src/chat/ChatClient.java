b82cbc9aab0a20a3c732ba94f559bedde2baf16d
Zujiry <till.pohland@data-haven.de>
2017-05-17 12:49:04 +0200

    Strings für die anfragen verändert Error # dinger rausgenommen Chatmessages werden nur noch mit raum gesendet, wenn es wirkling um einen anderen Raum geht. Eigentlich müssen wir das im Client gar nicht mehr machen? Help im client implementiert

--- a2-chat/src/chat/ChatClient.java
+++ a2-chat/src/chat/ChatClient.java
@@ -12,14 +12,15 @@ import java.util.Scanner;
 
 public class ChatClient {
     // Messages
-    public static String LOGOUT = "LOGOUT";
-    public static String LOGOUT_ROOM = "LOGOUT_ROOM";
-    public static String LIST_CHATROOMS = "CHATROOMS";
-    public static String LIST_USERS = "USERS";
-    public static String CREATE_CHATROOM = "CREATE";
-    public static String JOIN = "JOIN";
-    public static String IN_CHATROOM = "IN_CHATROOM";
-    public static String USERS_IN_CHATROOM = "USERS_CHAT";
+    public final static String LOGOUT = "LOGOUT";
+    public final static String LOGOUT_ROOM = "ROOMLOGOUT";
+    public final static String LIST_CHATROOMS = "CHATROOMS";
+    public final static String LIST_USERS = "ALLUSERS";
+    public final static String CREATE_CHATROOM = "CREATE";
+    public final static String JOIN = "JOIN";
+    public final static String IN_CHATROOM = "CURCHAT";
+    public final static String USERS_IN_CHATROOM = "CHASTUSERS";
+    public final static String HELP_SERVER = "HELPME";
     // for I/O
     private ObjectInputStream sInput;       // to read from the socket
     private ObjectOutputStream sOutput;     // to write on the socket
@@ -43,28 +44,28 @@ public class ChatClient {
             
             switch (msg.toUpperCase()) {
 
-                case "HELP_SERVER":
-                    sendMessage("Help please", ChatServer.GENERAL_CHAT_ROOM, ChatMessage.HELP_SERVER);
+                case HELP_SERVER:
+                    printHelp(false);
                     break;
-	            case "LOGOUT": // logout if message is LOGOUT
+	            case LOGOUT: // logout if message is LOGOUT
 	            	logout();
 	            	break;
-	            case "LOGOUT_ROOM": // logout if message is LOGOUT
+	            case LOGOUT_ROOM: // logout if message is LOGOUT
 	            	logoutRoom();
 	            	break;
-	            case "IN_CHATROOM": 
-	            	sendMessage("which chatroom am I in?", ChatServer.GENERAL_CHAT_ROOM, ChatMessage.IN_CHATROOM);
+	            case IN_CHATROOM:
+	            	sendMessage("which chatroom am I in?", ChatMessage.IN_CHATROOM);
 	            	break;
-	            case "USERS_IN_CHATROOM": 
-	                sendMessage("which chatroom am I in?", ChatServer.GENERAL_CHAT_ROOM, ChatMessage.USERS_IN_CHATROOM);
+	            case USERS_IN_CHATROOM:
+	                sendMessage("which chatroom am I in?", ChatMessage.USERS_IN_CHATROOM);
 	            	break;
-	            case "LIST_USERS":
-                    sendMessage(username + " requesting list of users", ChatServer.GENERAL_CHAT_ROOM, ChatMessage.LIST_USERS);
+	            case LIST_USERS:
+                    sendMessage(username + " requesting list of users", ChatMessage.LIST_USERS);
 	            	break;
-	            case "LIST_CHATROOMS":
+	            case LIST_CHATROOMS:
 	            	getChatrooms();
 	            	break;
-	            case "CREATE_CHATROOM":
+	            case CREATE_CHATROOM:
 	            	System.out.print("Write down the name for your new chatroom > ");
 	                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	                
@@ -74,12 +75,12 @@ public class ChatClient {
 	                    e.printStackTrace();
 	                }
 	                // Set roomname if message could be sent
-	                if (sendMessage(roomname, ChatServer.GENERAL_CHAT_ROOM, ChatMessage.CREATE_CHATROOM)) {
+	                if (sendMessage(roomname, ChatMessage.CREATE_CHATROOM)) {
 	                    oldChatRoom = chatRoom;
 	                    chatRoom = roomname;
 	                }
 	                break;
-	            case "JOIN":
+	            case JOIN:
 	            	getChatrooms();
 	            	System.out.print("Which chatroom do you want to join? > ");
 	                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -88,7 +89,8 @@ public class ChatClient {
 	                } catch (IOException e) {
 	                    e.printStackTrace();
 	                }
-	                sendMessage(roomname, ChatServer.GENERAL_CHAT_ROOM, ChatMessage.JOIN_CHATROOM);
+                    // TODO: Chatroom gets written here but is wrong if the chatroom does not exist!
+	                sendMessage(roomname, ChatMessage.JOIN_CHATROOM);
 	                oldChatRoom = chatRoom;
 	                chatRoom = roomname;
 	                break;
@@ -102,7 +104,7 @@ public class ChatClient {
     }
     
     public void getChatrooms() {
-    	sendMessage(username + " requesting list of chatrooms", ChatServer.GENERAL_CHAT_ROOM, ChatMessage.LIST_CHATROOMS);
+    	sendMessage(username + " requesting list of chatrooms", ChatMessage.LIST_CHATROOMS);
     }
 
     public boolean login(String server, int serverPort){
@@ -139,9 +141,10 @@ public class ChatClient {
             System.out.print("Username eingeben > ");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             username = br.readLine();
-            sendMessage(username, ChatServer.GENERAL_CHAT_ROOM, ChatMessage.INITIALIZE);
+            sendMessage(username, ChatMessage.INITIALIZE);
         
             System.out.println("@Username saved and logged in");
+            printHelp(true);
         } catch (IOException eIO) {
             logout();
             return false;
@@ -151,7 +154,7 @@ public class ChatClient {
     
     private void logout() {
         if(sOutput != null && socket != null ) {
-            sendMessage("", ChatServer.GENERAL_CHAT_ROOM, ChatMessage.LOGOUT);
+            sendMessage("", ChatMessage.LOGOUT);
         }
 
         try {
@@ -172,8 +175,29 @@ public class ChatClient {
         runningClient = false;
     }
 
+    private void printHelp(boolean initialization){
+        String helpString = "\n ### Command help ###" +
+                "\n (you can type in whatever case you want) " +
+                "\nType: " + LOGOUT +" to logout of the server" +
+                "\nType: " + CREATE_CHATROOM +" to create a chatroom on the server you are logged in to" +
+                "\nType: " + JOIN +" to join a chatroom" +
+                "\nType: " + LIST_CHATROOMS +" to list all available chatrooms" +
+                "\nType: " + LIST_USERS +" to list all users on the server" +
+                "\nType: " + USERS_IN_CHATROOM +" to list all users in the chatroom you are currently in" +
+                "\nType: " + IN_CHATROOM +" to display the chatroom you are currently in" +
+                "\nType: " + LOGOUT +" to join a chatroom" +
+                "\nType: " + LOGOUT_ROOM +" to logout of the current chatroom and into the general chatroom" +
+                "\nType: " + HELP_SERVER +" to display this help" +
+                "\n ### End ###\n";
+
+        System.out.println(helpString);
+        if(!initialization) {
+            System.out.print(chatRoom.toString() + " > ");
+        }
+    }
+
     private void logoutRoom() {
-    	boolean msg = sendMessage("", ChatServer.GENERAL_CHAT_ROOM, ChatMessage.CHATROOM_LOGOUT);
+    	boolean msg = sendMessage("", ChatMessage.CHATROOM_LOGOUT);
     	
     	if(msg) {
     		System.out.println("Logout war Erfolgreich");
@@ -182,6 +206,18 @@ public class ChatClient {
     	}
     }
     
+    public boolean sendMessage(String message, int type) {
+        ChatMessage cm = new ChatMessage(message, type);
+
+        try {
+            sOutput.writeObject(cm);
+            return true;
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     public boolean sendMessage(String message, String room, int type) {
         ChatMessage cm = new ChatMessage(message, room, type);
 
@@ -206,38 +242,9 @@ public class ChatClient {
                 try {
                     String msg = (String) sInput.readObject();
                     System.out.println(msg);
-                    
-                    if(realMsg){
-                        System.out.println(msg);
-                        realMsg = false;
-                    } else if(error) {
-                        System.out.println(msg);
-                        chatRoom = oldChatRoom;
-                        error = false;
-                    }else if(serverCall) {
-                        System.out.println(msg);
-                        serverCall = false;
-                    }else if(username_error) {
-                        System.out.println(msg);
-                        username_error = false;
-                    } else if(msg.equals(ChatServer.ERR_USERNAME)) {
-                        username_error = true;
-                        continue;
-                    } else if(msg.equals(ChatServer.MESSAGE_FROM_OTHER_CLIENT)) {
-                        realMsg = true;
-                        continue;
-                    } else if (msg.equals(ChatServer.SERVER_CALL)){
-                        serverCall = true;
-                        continue;
-                    } else if (msg.equals(ChatServer.ERROR)){
-                        error = true;
-                        continue;
-                    } else if (msg.equals(ChatServer.CHATROOM_LOGOUT)){
-                        error = true;
-                        continue;
-                    } else {
-                    }
+                    System.out.print(chatRoom.toString() + " > ");
                 } catch (IOException e) {
+                    running = false;
                     e.printStackTrace();
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
